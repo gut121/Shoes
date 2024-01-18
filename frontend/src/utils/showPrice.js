325b7f2341d1a042c655eea1231126448380192e
minhkhang201 <trmkhang@gmail.com>
2024-01-18 15:11:56 +0700

    push source code

---
+++ frontend/src/utils/showPrice.js
@@ -0,0 +1,10 @@
+const showPrice = (price) => {
+    // price=price+'.000'
+    // return price?.toLocaleString("it-IT", {
+    //   style: "currency",
+    //   currency: "VND",
+    // }
+    // );
+    return price
+  };
+  export default showPrice
